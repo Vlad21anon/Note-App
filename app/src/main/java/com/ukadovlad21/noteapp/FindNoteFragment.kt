7316797f460d8cc0939b71b73c03ba432b729b2a
Vlad Legkiy <ukadoV@gmail.com>
2023-02-11 12:08:15 +0200

    redesign toolbar_find.xml and made automatic input in EditText

--- app/src/main/java/com/ukadovlad21/noteapp/FindNoteFragment.kt
+++ app/src/main/java/com/ukadovlad21/noteapp/FindNoteFragment.kt
@@ -1,17 +1,37 @@
 package com.ukadovlad21.noteapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import com.ukadovlad21.noteapp.databinding.FragmentFindNoteBinding
+
 class FindNoteFragment : Fragment() {
+    private lateinit var binding: FragmentFindNoteBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find_note, container, false)
+        binding = FragmentFindNoteBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        makeFocusOnEditText(requireContext(), binding.toolbarFind.tietSearch)
+
+    }
+
+    private fun makeFocusOnEditText(context: Context, editText: EditText) {
+        editText.requestFocus()
+
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+    }
 }
+
