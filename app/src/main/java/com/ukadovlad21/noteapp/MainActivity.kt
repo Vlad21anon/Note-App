72e34d29e13a26aab125e1b612fe24501f6a9022
Vlad Legkiy <ukadoV@gmail.com>
2023-02-11 12:06:58 +0200

    created navigation

--- app/src/main/java/com/ukadovlad21/noteapp/MainActivity.kt
+++ app/src/main/java/com/ukadovlad21/noteapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ukadovlad21.noteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
+    val navController by lazy {
+        Navigation.findNavController(this, R.id.noteNavHostFragment)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
